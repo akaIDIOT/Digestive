aa3a2f21567fbb25822144e66a02cc9d1080cf80
Mattijs Ugen <mattijsugen@gmail.com>
2014-09-17 20:09:56 +0200

    Add help strings to commandline arguments

--- digestive/main.py
+++ digestive/main.py
@@ -19,25 +19,33 @@ def file_size(size):
 
 
 def parse_arguments(arguments=None):
-    parser = ArgumentParser()
+    parser = ArgumentParser(description='run multiple digests on files')
     # hash digest sinks
-    parser.add_argument('-m', '--md5', action='append_const', dest='sinks', const=MD5)
-    parser.add_argument('-1', '--sha1', action='append_const', dest='sinks', const=SHA1)
-    parser.add_argument('-2', '--sha256', action='append_const', dest='sinks', const=SHA256)
-    parser.add_argument('-5', '--sha512', action='append_const', dest='sinks', const=SHA512)
+    parser.add_argument('-m', '--md5', action='append_const', dest='sinks', const=MD5,
+                        help='calculate MD5 hash')
+    parser.add_argument('-1', '--sha1', action='append_const', dest='sinks', const=SHA1,
+                        help='calculate SHA-1 hash')
+    parser.add_argument('-2', '--sha256', action='append_const', dest='sinks', const=SHA256,
+                        help='calculate SHA-256 hash')
+    parser.add_argument('-5', '--sha512', action='append_const', dest='sinks', const=SHA512,
+                        help='calculate SHA-512 hash')
     # convenience switch to include all hashes
-    parser.add_argument('--hashes', action='store_const', dest='sinks', const=(MD5, SHA1, SHA256, SHA512))
+    parser.add_argument('--hashes', action='store_const', dest='sinks', const=[MD5, SHA1, SHA256, SHA512],
+                        help='calculate MD5, SHA-1, SHA-256 and SHA-512 hashes (equivalent to -m125)')
     # entropy sink
-    parser.add_argument('-e', '--entropy', action='append_const', dest='sinks', const=Entropy)
+    parser.add_argument('-e', '--entropy', action='append_const', dest='sinks', const=Entropy,
+                        help='calculate binary entropy')
     # misc options
-    parser.add_argument('-j', '--jobs', type=int)
-    # TODO: -b, --block-size
+    parser.add_argument('-j', '--jobs', type=int, metavar='JOBS',
+                        help='use up to %(metavar)s threads to process digests (defaults to the number of digests)')
+    # TODO: -b, --block-size (with support suffixes like M, Ki, likely all mapping to binary)
     # TODO: -t, --time
     # TODO: -p, --progress
     # TODO: refuse --progress when outputting to a dumb terminal
     # TODO: -r, --recursive
     # positional arguments: sources
-    parser.add_argument('sources', metavar='FILE', nargs='+')
+    parser.add_argument('sources', metavar='FILE', nargs='+',
+                        help='input files')
 
     arguments = parser.parse_args(arguments)
     process_arguments(arguments, parser)
