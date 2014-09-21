8947a45b1bdbed2344b60e27d2552da245c8489b
Mattijs Ugen <mattijsugen@gmail.com>
2014-09-21 17:46:28 +0200

    Install decorator before before py.test run for travis

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ from setuptools import find_packages, setup
 import digestive
 
 
-requires = ['decorator']
-
 setup(
     name='digestive',
     version=digestive.__version__,
@@ -24,8 +22,8 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
-    install_requires=requires,
-    tests_require=requires + ['pytest', 'mock', 'decorator'],
+    install_requires=['decorator'],
+    tests_require=['pytest', 'mock'],
     entry_points={
         'console_scripts': {
             'digestive = digestive.main:main'
