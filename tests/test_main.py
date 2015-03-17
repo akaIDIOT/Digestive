5885e2f3b0657822fd8ddce14b2008e2f1366c5b
Mattijs Ugen <mattijsugen@gmail.com>
2015-03-17 22:11:06 +0100

    Test output to file being treated as a collector of dicts
    Fixes #7

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,7 @@ from argparse import Namespace
 from concurrent.futures import ThreadPoolExecutor
 from os import path
 
-from mock import Mock, patch
+from mock import ANY, call, MagicMock, Mock, patch
 import pytest
 
 from digestive.entropy import Entropy
@@ -125,24 +125,46 @@ def test_process_source():
 
 
 def test_main():
-    with patch('builtins.print') as mocked:
-        arguments = ['--hashes', path.join(here, 'files/empty'), path.join(here, 'files/1234')]
+    with patch('builtins.print') as mocked_print:
+        arguments = ['--hashes', '--output', '/dev/null', path.join(here, 'files/empty'), path.join(here, 'files/1234')]
         main(arguments)
         # assert a few of the prints that should have been made
-        mocked.assert_any_call('{} ({})'.format(path.join(here, 'files/empty'), '0 bytes'), flush=True)
-        mocked.assert_any_call('  sha256       e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855')
-        mocked.assert_any_call('{} ({})'.format(path.join(here, 'files/1234'), '4 bytes'), flush=True)
-        mocked.assert_any_call('  sha256       9f64a747e1b97f131fabb6b447296c9b6f0201e79fb3c5356e6c77e89b6a806a')
+        mocked_print.assert_any_call('{} ({})'.format(path.join(here, 'files/empty'), '0 bytes'), flush=True)
+        mocked_print.assert_any_call('  sha256       e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855')
+        mocked_print.assert_any_call('{} ({})'.format(path.join(here, 'files/1234'), '4 bytes'), flush=True)
+        mocked_print.assert_any_call('  sha256       9f64a747e1b97f131fabb6b447296c9b6f0201e79fb3c5356e6c77e89b6a806a')
 
-        mocked.reset_mock()
-
+    with patch('builtins.print'), patch('digestive.main.output_to_file') as output:
+        output_generator = MagicMock()
+        output.return_value = output_generator
         # force raw format to treat random.E01 as a regular file (courtesy of Travis…)
-        arguments = ['--hashes', '-f', 'raw', '--recursive', path.join(here, 'files')]
+        arguments = ['--hashes', '-f', 'raw', '--recursive', '--output', '/dev/null', path.join(here, 'files')]
         main(arguments)
-        # assert recursing into files and processing the test files
-        mocked.assert_any_call('{} ({})'.format(path.join(here, 'files/empty'), '0 bytes'), flush=True)
-        mocked.assert_any_call('  sha256       e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855')
-        mocked.assert_any_call('{} ({})'.format(path.join(here, 'files/1234'), '4 bytes'), flush=True)
-        mocked.assert_any_call('  sha256       9f64a747e1b97f131fabb6b447296c9b6f0201e79fb3c5356e6c77e89b6a806a')
-        mocked.assert_any_call('{} ({})'.format(path.join(here, 'files/random.dd'), '1 MiB'), flush=True)
-        mocked.assert_any_call('  sha256       810ec5f2086379f0e8000456dbf2aede8538fbc9d9898835f114c8771ed834b5')
+        # assert recursing into files and processing the test files, posting results to output
+        output.assert_called_with('/dev/null')
+        output_generator.send.assert_has_calls([
+            # initial info call
+            call({'digestive': '0.1', 'started': ANY}),
+            # hashes of tests/files/1234
+            call({
+                'source': ANY,
+                'size': 4,
+                'completed': ANY,
+                'md5': '08d6c05a21512a79a1dfeb9d2a8f262f',
+                'sha1': '12dada1fff4d4787ade3333147202c3b443e376f',
+                'sha256': '9f64a747e1b97f131fabb6b447296c9b6f0201e79fb3c5356e6c77e89b6a806a',
+                'sha512': 'a7c976db1723adb41274178dc82e9b777941ab201c69de61d0f2bc6d27a3598f594fa748e50d88d3c2bf1e2c2e72c3cfef78c3c6d4afa90391f7e33ababca48e',
+                'sha3-256': 'a6885b3731702da62e8e4a8f584ac46a7f6822f4e2ba50fba902f67b1588d23b',
+            }),
+            # hashes of tests/files/random.dd
+            call({
+                'source': ANY,
+                'size': 1048576,
+                'completed': ANY,
+                'md5': '257f5c2913ea856cb0a2313f167452d4',
+                'sha1': '2f8a9e749cc8e46bebe602827228e76611346f54',
+                'sha256': '810ec5f2086379f0e8000456dbf2aede8538fbc9d9898835f114c8771ed834b5',
+                'sha512': '24dbb6cb56757a621fb8e6a8c8733f1cfc3c77bd23ac325e672eaaf856eac602307541ac434f598afb62448e90b3608344cfeb2e64778d3f7024bc69f5bb46ef',
+                'sha3-256': '8f33c358764548766d12885d06aea06d00469b2828cefb516b8f5aedc2826352'
+            })
+        ], any_order=True)
